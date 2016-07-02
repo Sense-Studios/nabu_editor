50ce0f04c8cd01857f6b8436b95d8ea28a7616d2
Sense <urias@sense-studios.com>
2016-07-03 01:59:48 +0200

    some interface razzle dazzle

--- app/assets/javascripts/nabu_editor/editor/user_interface.js
+++ app/assets/javascripts/nabu_editor/editor/user_interface.js
@@ -52,13 +52,18 @@ var createTrackEvent = function( marqer, trackline, select_id ) {
   var html = '';
   html += '<div class="trackeventcontainer">';
   html += ' <div class="marqer_item trackevent btn-material-burgundy-transperant" alt="'+name+'" title="'+name+'" data-remote_id="'+remote_id+'" id="'+ ids + '" data-type='+type+' data-name="'+name+'" style="left: '+l+'; width: '+w+';">';
+
   html += '  <div class="tooltip-left"></div>';
   html += '  <div class="tooltip-right"></div>';
+
   html += '  <ul class="nav nav-tabs">';
   html += '   <li class="dropdown">';
-  html += '    <span class="glyphicon glyphicon-menu-hamburger" aria="" hidden="true"></span>';
-  html += '     <a class="dropdown-toggle" data-toggle="dropdown" href="javascript:"></a>';
+  html += '     <a class="dropdown-toggle tracker-dropdown" data-toggle="dropdown" href="javascript:">';
+  html += '       <span class="glyphicon glyphicon-menu-hamburger" aria="" hidden="true"></span>';
+  html += '     </a>';
+
   html += '     <ul class="dropdown-menu dropdown-menu-editor">';
+  html += '       <li class="drowndown-menu-title">' + name + "</li>"
 
   if ( marqer.marqeroptions.position != undefined && marqer.marqeroptions.original != undefined ) {
     html += '     <li><a class="position_button" data-remote_id="'+remote_id+'" href="javascript:"><span class="glyphicon glyphicon-modal-window ignore_events"/>' + t.user_interface.place + '</a></li>';
@@ -68,6 +73,8 @@ var createTrackEvent = function( marqer, trackline, select_id ) {
   html += '      <li><a class="stratum_button" onClick="$(this).closest(\'.dropdown-toggle\').hide()"  data-remote_id="'+remote_id+'" href="javascript:"><span class="glyphicon glyphicon-save ignore_events"/>' + t.user_interface.prefab + '</a></li>';
   html += '      <li><a class="delete_button" onClick="$(this).closest(\'.dropdown-toggle\').hide()"  data-remote_id="'+remote_id+'" href="javascript:"><span class="glyphicon glyphicon-trash ignore_events"/>' + t.user_interface.delete + '</a></li>';
   html += '     </ul>';
+
+
   html += '    </li>';
   html += '   </ul>';
   // if marqer.track.hasposition?
